1527afc59f8fc404b3741f14751d900697cce64e
Sara Hamilton <sara@indeterminate.net>
2018-01-12 09:59:41 -0800

    add basic javascript functionality to display survey result

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 $ (document).ready(function(){
   $("#trackForm").submit(function(event){
+
     var companySizeAnswer = $("input:radio[name=companySize]:checked").val();
     var frontOrBackAnswer = $("input:radio[name=frontOrBack]:checked").val();
     var mobileAnswer = $("input:radio[name=mobile]:checked").val();
     var interactiveAnswer = $("input:radio[name=interactive]:checked").val();
     var demandAnswer = $("input:radio[name=demand]:checked").val();
     var osPreferenceAnswer = $("input:radio[name=osPreference]:checked").val();
+
+  if (frontOrBackAnswer === "front") {
+    var trackSuggestion = "CSS/Design"
+  }
+
+  $("#track").empty().append(trackSuggestion);
+  $("#result").show();
+
+  event.preventDefault();
   });
 });
