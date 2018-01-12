ce3de16e6884208de69e8b302006b0f28d99913a
Sara Hamilton <sara@indeterminate.net>
2018-01-12 11:09:08 -0800

    add if else statements to determine track suggestion

--- js/scripts.js
+++ js/scripts.js
@@ -12,54 +12,61 @@ $ (document).ready(function(){
     var java = 0;
     var php = 0;
     var ruby = 0;
-    var design = 0;
 
-    if (companySizeAnswer = "large") {
+    if (companySizeAnswer === "large") {
       cSharp +=1, java +=1, php +=1
     }
-    if (companySizeAnswer = "small") {
+    if (companySizeAnswer === "small") {
+      ruby +=2
+    }
+
+    if (frontOrBackAnswer === "back") {
+      php +=2
+    }
+
+    if (mobileAnswer === "yes") {
+      java +=2
+    }
+
+    if (interactiveAnswer === "yes") {
       ruby +=1
     }
-    if (companySizeAnswer = "either") {
-      cSharp +=1, java +=1, php +=1, ruby +=1
-    }
-
-    if (frontOrBackAnswer = "front") {
-      design +=10
-    }
-
-    if (mobileAnswer = "yes") {
-      java +=1
-    }
-
-    if (interactiveAnswer = "yes") {
-      ruby +=1
-    }
-    if (interactiveAnswer = "no") {
+    if (interactiveAnswer === "no") {
       ruby -=1
     }
 
-    if (demandAnswer = "yes") {
+    if (demandAnswer === "yes") {
       cSharp +=1, java +=1, php +=1
     }
-    if (demandAnswer = "no") {
+    if (demandAnswer === "no") {
       ruby +=1
     }
 
-    if (osPreferenceAnswer = "mac") {
+    if (osPreferenceAnswer === "mac") {
       ruby +=1
     }
-    if (osPreferenceAnswer = "windows") {
-      cSharp +=1
+    if (osPreferenceAnswer === "windows") {
+      cSharp +=2
     }
 
-
-
-
+    var highestValue = Math.max(cSharp, php, ruby, java);
 
     if (frontOrBackAnswer === "front") {
       var trackSuggestion = "CSS/Design"
+    } else if (cSharp === highestValue) {
+      var trackSuggestion = "C#"
+    } else if (java === highestValue) {
+      var trackSuggestion = "Java"
+    } else if (php === highestValue) {
+      var trackSuggestion = "PHP"
+    } else {
+      var trackSuggestion = "Ruby"
     }
+
+
+    console.log(cSharp, php, ruby, java)
+    console.log(companySizeAnswer)
+    console.log(Math.max(cSharp, php, ruby, java))
 
     $("#track").empty().append(trackSuggestion);
     $("#result").show();
